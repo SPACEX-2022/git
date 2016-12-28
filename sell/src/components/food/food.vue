9fc4f6a5db5adc700653d51bee43459b4633aa0d
duan <540240126@qq.com>
2016-12-28 11:39:19 +0800

    完善商品详情

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -16,18 +16,25 @@
 				<div class="price">
 					<span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
 				</div>
+				<div class="cartcontrol-wrapper">
+					<cartcontrol :food="food"></cartcontrol>
+				</div>
+				<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0" transition="fade">加入购物车</div>
 			</div>
-			<div class="cartcontrol-wrapper">
-				<cartcontrol :food="food"></cartcontrol>
+			<split v-show="food.info"></split>
+			<div class="info" v-show="food.info">
+				<h1 class="title">商品信息</h1>
+				<p class="text">{{food.info}}</p>
 			</div>
-			<div class="buy"></div>
 		</div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
 	import BScroll from 'better-scroll';
+	import Vue from 'vue';
 	import cartcontrol from 'components/cartcontrol/cartcontrol';
+	import split from 'components/split/split';
 
 	export default {
 		props: {
@@ -55,10 +62,18 @@
 			},
 			hide() {
 				this.showFlag = false;
+			},
+			addFirst(event) {
+				if (!event._constructed) {
+					return;
+				}
+				this.$dispatch('cart.add', event.target);
+				Vue.set(this.food, 'count', 1);
 			}
 		},
 		components: {
-			cartcontrol
+			cartcontrol,
+			split
 		}
 	};
 </script>
@@ -98,6 +113,7 @@
 					font-size: 20px
 					color: #fff
 		.content
+			position: relative
 			padding: 18px
 			.title
 				line-height: 14px
@@ -126,4 +142,38 @@
 					text-decoration: line-through
 					font-size: 10px
 					color: rgb(147, 153, 159)
+			.cartcontrol-wrapper
+				position: absolute
+				right: 12px
+				bottom: 12px
+			.buy
+				position: absolute
+				right: 18px
+				bottom: 18px
+				z-index: 10
+				height: 24px
+				line-height: 24px
+				padding: 0 12px
+				box-sizing: border-box
+				border-radius: 12px
+				font-size: 10px
+				color: #fff
+				background: rgb(0, 160, 220)
+				&.fade-transition
+					transition: all 0.2s
+					opacity: 1
+				&.fade-enter,&.fade-leave
+					opacity: 0
+		.info
+			padding: 18px
+			.title
+				line-height: 14px
+				margin-bottom: 6px
+				font-size: 14px
+				color: rgb(7, 17, 27)
+			.text
+				line-height: 24px
+				padding: 0 8px
+				font-size: 12px
+				color: rgb(77, 85, 93)
 </style>
